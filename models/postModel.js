688cb5e10443aeaebf9c51e3e6523f2edcf24f20
nooorf <nfatima.bscs23seecs@seecs.edu.pk>
2025-02-24 10:04:57 +0500

    Updated post model

--- models/postModel.js
+++ models/postModel.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 const postSchema = new mongoose.Schema({
     title: {type: String, required: true},
+    slug: {type: String, required: true},
     description: {type: String, required: true},
     category: { type: String },
     githubRepo: String,
+    image: {type: URL, default: "https://via.placeholder.com/150"},
+    pitch: {type: String, required: true},
     user: { type: mongoose.Schema.Types.ObjectId, ref: "users" }, 
     tags: [{ type: String, lowercase: true, trim: true }],
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }], 
