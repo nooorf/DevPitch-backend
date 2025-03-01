29e1572e50ad54f3ba8916e97675636790c79ae5
nooorf <nfatima.bscs23seecs@seecs.edu.pk>
2025-03-01 11:01:10 +0500

    Fixed issue with verifyToken middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   try {
-  const token = req.cookies.authToken || req.headers.authorization?.split("")[1];
+  const token = req.cookies.authToken || req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized - No token provided" });
