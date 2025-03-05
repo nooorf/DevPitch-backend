f7e6ee26ca5cd7c9ebb681da669ea15f51ef91ef
nooorf <nfatima.bscs23seecs@seecs.edu.pk>
2025-03-05 19:31:56 +0500

    Added backend route to fetch posts based on user id

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,4 +8,18 @@ router.get("/", async (req, res) => {
     res.json(userData);
 });
 
+//fetch posts based on user id
+router.get("/:id/posts", async (req, res) => {
+    try {
+      const { id } = req.params;
+      
+      const posts = await PostModel.find({ user: id }).populate("user", "name email"); 
+  
+      res.status(200).json(posts);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+      res.status(500).json({ message: "Server error" });
+    }
+  });
+
 export default router;
